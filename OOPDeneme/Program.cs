f4ef3908d44a05e5896c5f1d7c6d40439ba20a03
khas <khas@MYLABPC-D05>
2022-12-28 14:58:07 +0300

    Add project files.

---
+++ OOPDeneme/Program.cs
@@ -0,0 +1,37 @@
+﻿using OOPDeneme;
+
+internal class Program
+{
+    //insan isimli bir sınıf içinde boy kilo ve yaş alanlarını tanımlayıp,
+    //bir kurucu metot içinde bu alanlar için varsayılan değer atamaları yapacağız 
+    //Son olarak "Tanis" isimli metot ile girilen bilgileri kullanıcıya belirli 
+    //Açıklayıcı bir şekilde görüntüleyen bir program yazınız.
+    private static void Main(string[] args)
+    {
+        int kkilo=0;
+        int kyas = 0;
+        int kboy = 0;
+        string kad="";
+        string ksoyad = "";
+        Console.WriteLine("Adınızı girinz:");
+        kad = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Soyadınızı girinz:");
+        ksoyad = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Kilonuzu girinz:" );
+        kkilo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Yaşınızı girinz:");
+        kyas = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Boyunuzu girinz:");
+        kboy = Convert.ToInt32(Console.ReadLine());
+
+        
+
+        new clsOzellik(kkilo,kyas,kboy,kad,ksoyad);
+
+
+    }
+}
---
+++ OOPDeneme/clsOzellik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDeneme
+{
+    internal class clsOzellik
+    {
+        public string Ad { get; set; }  
+        public string Soyad { get; set; }
+        public int Boy { get; set; }
+        public int Kilo { get; set; }
+        public int Yas{ get; set; }
+
+        public clsOzellik(int boy,int kilo,int yas, string ad,string soyad)
+
+        {
+            Soyad = soyad;
+            Ad = ad;
+            Boy = boy;
+            Kilo = kilo;
+            Yas= yas;
+
+
+            Console.WriteLine(" Adınız: "+Ad+" Soyadınız: "+Soyad+ "\n Boyunuz: " + Boy + " Kilonuz: " + kilo + " Yaşınız: " + Yas );
+        }
+    }
+}
